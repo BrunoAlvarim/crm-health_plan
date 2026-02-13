56d5e770b4f9ac22a81631b2629cfbb9bdb8747d
BrunoAlvarim <brunoalvarim1@gmail.com>
2026-02-13 16:31:53 -0300

    Subindo Api

--- api/serializers.py
+++ api/serializers.py
@@ -10,4 +10,18 @@ class PlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Plan
         exclude = ['created_at', 'updated_at','is_active','row_id']
-    
+
+class LeadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Lead
+        exclude = ['created_at', 'updated_at','is_active','row_id']
+
+class OpportunitiesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Opportunities
+        exclude = ['created_at', 'updated_at','is_active','row_id']
+
+class SalesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Sales
+        exclude = ['created_at', 'updated_at','is_active','row_id']
